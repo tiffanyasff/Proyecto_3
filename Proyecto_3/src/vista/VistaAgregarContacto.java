7b922caec2ab331a562e25e7fa58d570a24baaa7
Pablo Becerra <pabloestebanbecerra@gmail.com>
2023-11-16 23:32:37 -0500

    creacion de la vista de creacion

--- Proyecto_3/src/vista/VistaAgregarContacto.java
+++ Proyecto_3/src/vista/VistaAgregarContacto.java
@@ -16,6 +16,7 @@ public class VistaAgregarContacto extends JFrame {
     private JFrame ventanaAgregar;
     private JTextField nombreTextField, apellidoTextField, tipoContactoTextField, fechaNacimientoTextField, identificacionTextField, lugarAsociadoTextField, direccionTextField;
     private JTextArea direccionesTextArea;
+    private JButton btnCrearContacto, btnAgregarDireccion;
 
     public VistaAgregarContacto() {
         ventanaAgregar = new JFrame("Agregar contacto");
@@ -27,7 +28,7 @@ public class VistaAgregarContacto extends JFrame {
     public void iniciarComponentes() {
         JPanel panel = new JPanel();
         panel.setLayout(null);
-
+        
         // Etiquetas
         JLabel nombreLabel = new JLabel("Nombre:");
         JLabel apellidoLabel = new JLabel("Apellido:");
@@ -36,7 +37,7 @@ public class VistaAgregarContacto extends JFrame {
         JLabel identificacionLabel = new JLabel("Identificación:");
         JLabel lugarAsociadoLabel = new JLabel("Lugar Asociado:");
         JLabel direccionesLabel = new JLabel("Direcciones:");
-
+        
         // Campos de texto
         nombreTextField = new JTextField();
         apellidoTextField = new JTextField();
@@ -47,13 +48,46 @@ public class VistaAgregarContacto extends JFrame {
         direccionTextField = new JTextField();
         direccionesTextArea = new JTextArea();
         
-        nombreLabel.setBounds(250, 100, 60, 60);
-        apellidoLabel.setBounds(250, 200, 60, 60);
-        tipoContactoLabel.setBounds(250, 300, 60, 60);
-        nombreLabel.setBounds(250, 400, 60, 60);
-        nombreLabel.setBounds(250, 500, 60, 60);
-        nombreLabel.setBounds(250, 600, 60, 60);
-        nombreLabel.setBounds(250, 700, 60, 60);
+        nombreLabel.setBounds(40, 80, 150, 90);
+        apellidoLabel.setBounds(40, 160, 150, 90);
+        tipoContactoLabel.setBounds(40, 240, 150, 90);
+        fechaNacimientoLabel.setBounds(40, 325, 190, 90);
+        identificacionLabel.setBounds(40, 400, 150, 90);
+        lugarAsociadoLabel.setBounds(40, 480, 150, 90);
+        direccionesLabel.setBounds(40, 560, 150, 90);
+        
+        nombreTextField.setBounds(220, 120, 100, 20);
+        apellidoTextField.setBounds(220, 195, 100, 20);
+        tipoContactoTextField.setBounds(220, 275, 100, 20);
+        fechaNacimientoTextField.setBounds(220, 360, 100, 20);
+        identificacionTextField.setBounds(220, 440, 100, 20);
+        lugarAsociadoTextField.setBounds(220, 520, 100, 20);
+        direccionTextField.setBounds(220, 590, 100, 20);
+        
+        panel.add(nombreLabel);
+        panel.add(apellidoLabel);
+        panel.add(tipoContactoLabel);
+        panel.add(fechaNacimientoLabel);
+        panel.add(identificacionLabel);
+        panel.add(lugarAsociadoLabel);
+        panel.add(direccionesLabel);
+        
+        
+        panel.add(nombreTextField);
+        panel.add(apellidoTextField);
+        panel.add(tipoContactoTextField);
+        panel.add(fechaNacimientoTextField);
+        panel.add(identificacionTextField);
+        panel.add(lugarAsociadoTextField);
+        panel.add(direccionTextField);
+        
+        
+        
+        
+        
+        ventanaAgregar.add(panel);
+        panel.setVisible(true);
+        ventanaAgregar.setVisible(true);
 
     }
 }
